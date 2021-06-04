4870f2b07233a528ae58488c095162059579ecef
JotaMorales <jnmorales73@live.com>
2021-06-03 23:25:19 -0300

    linkeado con js

---
+++ index.js
@@ -0,0 +1,8 @@
+let ingreso = prompt('Te enteraste del Sale?');
+console.log(ingreso);
+if (ingreso == 'no') {
+    alert('Que estas esperando!?');
+} else if (ingreso == 'si') {
+    console.log(ingreso);
+    alert('Bienvenido!');
+}

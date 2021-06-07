9178f8b87e6b1d5777252740a4396ce5323872ba
JotaMorales <jnmorales73@live.com>
2021-06-06 23:23:00 -0300

    correccion de funciones

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-let ingreso = prompt('Te enteraste del Sale?');
-console.log(ingreso);
-if (ingreso == 'no') {
+let respuesta = prompt('Llego el HOT SALE!, encontraste lo que buscabas?');
+console.log(respuesta);
+if (respuesta == 'no') {
     alert('Que estas esperando!?');
-} else if (ingreso == 'si') {
-    console.log(ingreso);
+} else if (respuesta == 'si') {
+    console.log(respuesta);
     alert('Bienvenido!');
 }
+
+
+function cuotas(valorTotal, cantidadCuotas) {
+    return valorTotal / cantidadCuotas;
+}
+if (respuesta == 'si') {
+
+}
+
+
+let totalIngresado = parseInt(prompt('¿Cual es el valor del producto?'));
+let cuotasIngresados = parseInt(prompt('¿Cuantas cuotas quieres?'));
+alert('El valor de cada cuota es $' + cuotas(totalIngresado, cuotasIngresados));

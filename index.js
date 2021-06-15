be88fa2d63fec83d00121d46c4fa4abda7fbbbce
JotaMorales <jnmorales73@live.com>
2021-06-14 22:55:54 -0300

    sort talle

--- index.js
+++ index.js
@@ -91,16 +91,21 @@ console.log(ArrayProductos);
 const nuevoArray = ArrayProductos.length;
 console.log(nuevoArray);
 
+
 const off = [producto7, producto8, producto9, producto19, producto20, producto21];
-//La función parámetro generalmente es una función flecha sin cuerpo.
-const encontrado = off.find(elemento => elemento > 3); //Encuentra 4
+console.log(off.length);
+
+off.sort(function(a, b) {
+    return a.talle - b.talle;
+})
+const encontrado = off.find(elemento => elemento.precio < 2100);
 console.log(off);
 
 
 
 
 
-producto1.agregar(Producto);
+producto12.agregar(Producto);
 
 ArrayProductos.sort(function(a, b) {
     return a.precio - b.precio
